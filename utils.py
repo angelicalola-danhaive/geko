504695b9fede0648ad83fa2a53369edd706fcaba
Lola Danhaive <angelicalola.danhaive@gmail.com>
2024-03-25 14:04:39 +0100

    Added functions
    added find threshold and make mask. moved compute PA here.

--- utils.py
+++ utils.py
@@ -12,6 +12,15 @@ import jax.numpy as jnp
 from jax.scipy.stats import norm
 
 
+from skimage.filters import threshold_otsu
+from skimage.morphology import closing
+from skimage.measure import label, regionprops
+from skimage.segmentation import clear_border
+from skimage.morphology import dilation, disk
+from skimage.color import label2rgb
+
+from matplotlib import pyplot as plt
+
 def oversample(image_low_res, factor, wave_factor, method='nearest'):
     image_high_res = image.resize(image_low_res, (int(
         image_low_res.shape[0]*factor), int(image_low_res.shape[1]*wave_factor)), method=method)
@@ -107,13 +116,84 @@ def find_best_sample(inference_data, variable, mu, sigma, max, min, MLS):
         inference_data.prior[var].data = scale_distribution(inference_data.prior[var].data, mu[i], sigma[i], max[i], min[i])
         
         if MLS == None:
-            print('Taking posterior median sample')
+            # print('Taking posterior median sample')
             best_sample.append(jnp.array(inference_data.posterior[var].median(dim=["chain", "draw"])))
         else:
-            print('Taking the maximum likelihood sample')
+            # print('Taking the maximum likelihood sample')
             best_sample.append(jnp.array(inference_data.posterior[var].isel(chain=MLS[0], draw=MLS[1])))
 
     
     return best_sample
 
 
+def make_mask(image, n, flux_threshold, dilation_factor):
+    """
+       Make n masks of the image, dilated by a factor dilation. If n =1, then flux_threshold
+       gives the threshold. If n>1, the threshold is set to the smallest number needed to obtain n
+       regions (using the find_threshold function).
+    """
+        
+    # threshold =threshold_otsu(image)
+    threshold = flux_threshold*image.max()
+    mask = jnp.zeros_like(image)
+    mask = mask.at[jnp.where(image>threshold)].set(1)
+    if n == 1:
+        mask = dilation(mask, disk(dilation_factor))
+        return mask
+    else:
+        masked_image = image*mask
+        #find the sub_regions only within the existing mask
+        threshold, masks = find_threshold(masked_image, n, dilation_factor)
+        return masks
+
+def find_threshold(image, n, dilation_factor):
+    """
+       Find the threshold needed to cut the image into n regions
+    """
+
+    #initialize the mask at all zeros so that the while loop runs at least once
+    label_image = jnp.zeros_like(image)
+    #start the guess at the threshold otsu value
+    threshold = threshold_otsu(image)
+    increment = threshold/10
+    while len(jnp.unique(label_image))-1 != n: #-1 because zeros count as one region too
+        threshold += increment
+        bw = closing(image > threshold)
+        cleared = clear_border(bw)
+        #label image regions
+        label_image = label(cleared)
+    #print the successful threshold
+
+    #cut the image into n regions
+    masks = []
+    for i in range(0,n):
+        mask = jnp.zeros_like(image)
+        mask = mask.at[jnp.where(label_image==i+1)].set(1)
+        mask = dilation(mask, disk(dilation_factor))
+        masks.append(mask)
+
+    return threshold, masks
+
+def compute_PA(image):
+	'''
+		compute PA of galaxy in the image using skimage measure regionprops
+
+	'''
+	threshold = threshold_otsu(image)
+	bw = closing(image > threshold)
+
+	cleared = clear_border(bw)
+
+	#label image regions
+	label_image = label(cleared)
+	image_label_overlay = label2rgb(label_image, image=image)
+	regions = regionprops(label_image, image)
+	PA = 90 + regions[0].orientation * (180/jnp.pi)
+	
+	return PA
+
+
+#things to add here
+#sampling uniform
+#creating x,y meshgrid (which takes 3 lines usually)
+#computing Bayes factyor
