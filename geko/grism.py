9249afc89b5a55495298c7937a4beca3ba99df59
Lola Danhaive <angelicalola.danhaive@gmail.com>
2026-02-23 12:07:33 +0100

    Improves wavelength solution accuracy
    Replaces nearest-neighbor interpolation with linear
    interpolation for calculating wavelengths. This prevents
    discrete jumps in the wavelength solution, reducing
    checkerboard artifacts.

--- geko/grism.py
+++ geko/grism.py
@@ -441,10 +441,9 @@ class Grism:
 			y_offset = (self.detector_space_1d_y - self.ycenter_detector)[:, jnp.newaxis]
 			dispersion_indices += y_offset
 
-		#for each dispersion offset, find the closest in the uniformly distributed dxs
-		wave_indices = np.argmin(np.abs(self.dxs[np.newaxis,np.newaxis,:] - dispersion_indices[:,:,np.newaxis]), axis = 2)
-		#translate this to a wavelength in the rest frame of the central pixel
-		self.wave_array = self.wavs[wave_indices]
+		# Use linear interpolation instead of nearest-neighbor argmin
+		# This prevents discrete jumps in wavelength solution that cause checkerboard artifacts
+		self.wave_array = np.interp(dispersion_indices.flatten(), self.dxs, self.wavs).reshape(dispersion_indices.shape)
 
 	def load_poly_factors(self,a01, a02, a03, a04, a05, a06, b01, b02, b03, b04, b05, b06, c01, c02, c03, d01):
 		"""
